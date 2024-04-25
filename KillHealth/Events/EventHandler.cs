c25a91d8ee9e5534361d6cf592d5d7c3ba686610
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2024-04-25 22:58:26 +0800

    Add project files.

---
+++ KillHealth/Events/EventHandler.cs
@@ -0,0 +1,14 @@
+﻿using Exiled.Events.EventArgs.Player;
+
+namespace KillHealth.Events;
+
+internal class EventHandler
+{
+    internal EventHandler(Configuration config) => LocalConfig = config;
+
+    Configuration LocalConfig;
+    public void PerformKillHandler(DiedEventArgs ev)
+    {
+        ev.Attacker.Health += LocalConfig.HealthToAdd;
+    }
+}
---
+++ KillHealth/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+
+namespace KillHealth;
+
+public class Plugin : Plugin<Configuration>
+{
+    public override string Name { get; } = "KillHealth";
+    public override string Author { get; } = "CypherTheProtogen";
+    public override Version Version { get; } = new Version(1, 0, 0);
+    public override string Prefix => "killhealth";
+
+    Events.EventHandler EVHandler; // Ignore the nullable.
+
+    public override void OnEnabled()
+    {
+        EVHandler = new Events.EventHandler(Config);
+
+        Exiled.Events.Handlers.Player.Died += EVHandler.PerformKillHandler;
+    }
+
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Player.Died -= EVHandler.PerformKillHandler;
+    }
+}
+
+public class Configuration : IConfig
+{
+    public bool IsEnabled { get; set; } = true;
+    public bool Debug { get; set; } = false;
+    public int HealthToAdd { get; set; } = 100;
+}
